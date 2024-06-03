f71ae4481fb7a5133e45228307b94a64ff8e8918
noor uzma <uzmasalmaan23@gmail.com>
2024-06-03 07:45:54 +0530

    added todo functionality

--- script.js
+++ script.js
@@ -13,13 +13,29 @@ function addTask(){
         li.appendChild(span);
     }
     inputBox.value="";
+    saveData();
 }
 
 listContainer.addEventListener("click",function(e){
     if(e.target.tagName==="LI"){
         e.target.classList.toggle("checked")
+        saveData();
     }else if(e.target.tagName==="SPAN"){
         e.target.parentElement.remove()
+        saveData();
 
     }
 },false)
+
+
+
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+
+function getData(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+getData();
